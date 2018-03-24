22b1c345000640ffad52441884203bcec662cb95
Zac Garby <me@zacgarby.co.uk>
2018-03-24 21:41:27 +0000

    Revert "Update README.md"
    This reverts commit 2ab65d4fce8aa2a337aa51bcfb3545c4d27fb7a3.

--- api/equation.go
+++ api/equation.go
@@ -1,11 +1,5 @@
 package api
 
-import (
-	"fmt"
-
-	"github.com/go-redis/redis"
-)
-
 // An Equation is a representation of an equation from the database.
 type Equation struct {
 	ID          int64    `json:"id"`
@@ -16,18 +10,4 @@ type Equation struct {
 	Score       int      `json:"score"`
 	Confirmed   bool     `json:"confirmed"`
 	Timestamp   int64    `json:"added"`
-}
-
-// FetchEquation fetches an equation from the database, by id.
-func FetchEquation(db *redis.Client, id int64) (*Equation, error) {
-	key := fmt.Sprintf("equation:%d", id)
-
-	val, err := db.HGetAll(key).Result()
-	if err != nil {
-		return nil, ErrDatabase
-	}
-
-	if len(val) == 0 {
-		return nil, ErrEquationNotExist
-	}
 }
