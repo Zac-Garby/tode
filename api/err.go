055ca427820b4c591bc36ecfe15dbde7ed7de2f1
Zac Garby <me@zacgarby.co.uk>
2018-03-25 11:52:07 +0100

    FetchEquation errors

--- api/err.go
+++ api/err.go
@@ -12,6 +12,11 @@ var (
 	ErrUserNotExist         = errors.New("user doesn't exist")
 	ErrUserInvalidTimestamp = errors.New("user has non-int timestamp")
 	ErrUserInvalidID        = errors.New("user has invalid ID")
+
+	ErrEquationNotExist         = errors.New("equation doesn't exist")
+	ErrEquationInvalidTimestamp = errors.New("equation has non-int timestamp")
+	ErrEquationInvalidScore     = errors.New("equation has non-int score")
+	ErrEquationInvalidAuthor    = errors.New("equation has non-int author")
 )
 
 func writeError(w http.ResponseWriter, e error) {
