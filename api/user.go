be2077dab4b9d77562540ee989b045daaf105140
Zac Garby <me@zacgarby.co.uk>
2018-03-25 12:01:52 +0100

    Store User.Equations as []int64

--- api/user.go
+++ api/user.go
@@ -7,13 +7,13 @@ import (
 
 // A User is an in-memory representation of a user from the database.
 type User struct {
-	ID        int64       `json:"id"`
-	Name      string      `json:"name"`
-	Email     string      `json:"-"`
-	Hash      string      `json:"-"`
-	Salt      string      `json:"-"`
-	Equations []*Equation `json:"equations"`
-	Timestamp int64       `json:"joined"`
+	ID        int64   `json:"id"`
+	Name      string  `json:"name"`
+	Email     string  `json:"-"`
+	Hash      string  `json:"-"`
+	Salt      string  `json:"-"`
+	Equations []int64 `json:"equations"`
+	Timestamp int64   `json:"joined"`
 }
 
 // FetchUser fetches a User from the database, by id. Returns an error if the
@@ -35,12 +35,18 @@ func (a *API) FetchUser(id int64) (*User, error) {
 		return nil, ErrUserInvalidTimestamp
 	}
 
+	equations, err := a.fetchEquationsOfUser(id)
+	if err != nil {
+		return nil, err
+	}
+
 	return &User{
 		ID:        id,
 		Name:      val["name"],
 		Email:     val["email"],
 		Hash:      val["hash"],
 		Salt:      val["salt"],
+		Equations: equations,
 		Timestamp: timestamp,
 	}, nil
 }
@@ -59,4 +65,26 @@ func (a *API) FetchUserByName(name string) (*User, error) {
 	}
 
 	return a.FetchUser(id)
+}
+
+func (a *API) fetchEquationsOfUser(id int64) ([]int64, error) {
+	key := fmt.Sprintf("user:%d:equations", id)
+
+	var equations []int64
+
+	val, err := a.db.SMembers(key).Result()
+	if err != nil {
+		return equations, ErrDatabase
+	}
+
+	for _, idStr := range val {
+		id, err := strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			return equations, err
+		}
+
+		equations = append(equations, id)
+	}
+
+	return equations, nil
 }
